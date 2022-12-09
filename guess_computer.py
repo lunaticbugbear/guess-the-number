a93a437972b93e89dfd50c7ef2ee9dad6baac6cc
lunaticbugbear <116770794+lunaticbugbear@users.noreply.github.com>
2022-12-09 10:10:11 +0700

    Create guess_computer.py
    Masukkan angka terbesar yang diinginkan: 100
    Saya sedang berpikir tentang sebuah angka antara 1 dan 100
    Tebakan komputer terlalu rendah. Mengubah range angka yang memungkinkan.
    Tebakan komputer terlalu rendah. Mengubah range angka yang memungkinkan.
    Tebakan komputer terlalu rendah. Mengubah range angka yang memungkinkan.
    Tebakan komputer terlalu tinggi. Mengubah range angka yang memungkinkan.
    Komputer berhasil menebaknya! Angka yang saya pikirkan adalah 48
    Komputer membutuhkan 5 tebakan untuk menebak angka yang benar.

---
+++ guess_computer.py
@@ -0,0 +1,39 @@
+import random
+
+# prompt the user to enter the maximum number that can be guessed
+max_num = int(input("Masukkan angka terbesar yang diinginkan: "))
+
+# randomly choose a number to be guessed
+number = random.randint(1, max_num)
+
+# set the initial number of guesses to zero
+num_guesses = 0
+
+# set the initial range of possible numbers to be all numbers between 1 and max_num
+low = 1
+high = max_num
+
+# prompt the user to start guessing
+print("Saya sedang berpikir tentang sebuah angka antara 1 dan", max_num)
+
+# keep looping until the computer guesses the correct number
+while True:
+  # have the computer make a guess
+  guess = (low + high) // 2
+
+  # increment the number of guesses
+  num_guesses += 1
+
+  # check if the guess is correct
+  if guess == number:
+    print("Komputer berhasil menebaknya! Angka yang saya pikirkan adalah", number)
+    print("Komputer membutuhkan", num_guesses, "tebakan untuk menebak angka yang benar.")
+    break
+
+  # give the computer a hint if its guess was too low or too high
+  elif guess < number:
+    print("Tebakan komputer terlalu rendah. Mengubah range angka yang memungkinkan.")
+    low = guess
+  elif guess > number:
+    print("Tebakan komputer terlalu tinggi. Mengubah range angka yang memungkinkan.")
+    high = guess
